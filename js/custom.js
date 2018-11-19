b3864ae1529ac81e18a7a53a5b3d82033aed9662
mackymiro <ask.macky.miro@gmail.com>
2018-11-19 15:56:29 +0800

    added fikes

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+(function(){
+
+	'use strict';
+
+	//Add payment method
+	$('body').on('click','.btn-add-method',function(e){
+
+		e.preventDefault();
+
+		var container = $('.additional-methods');
+		var content = $('.origin').html();
+			content = '<div class="row clone-method">' + content + '</div>';
+		
+		container.append(content);
+
+	});
+
+
+	//Remove payment method
+	$('body').on('click','.btn-remove-method',function(e){
+
+		e.preventDefault();
+		$(this).parents('.clone-method').remove();
+
+	});
+
+})(jQuery);
